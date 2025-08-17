c434532f115dd4256220c5e3ae2aceec47d01bd5
zeryab-afk <zaryabshafiq54@gmail.com>
2025-08-17 21:21:46 +0500

    dynamiccards

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,45 @@
-import "./WeatherCard.css"
-import cloudy from "../../assets/cloudy.png"
-function WeatherCard({weatherData}) {
-    return <section className="Weather-Card">
-        <p className="Weather-Card__temp">{weatherData.temp.F} &deg; F</p>
-        <img src={cloudy} alt="cloudy" className="Weather-Card__image" />
+import "./WeatherCard.css";
+import { weatherOptions, defaultweatherOptions } from "../../utils/constants";
+
+function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter(
+    (option) =>
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+  );
+
+  // declare all variables here so they exist in both cases
+  let weatherOptionUrl;
+  let weatherOptionCondition;
+  let weatherOptionDay;
+
+  if (filteredOptions.length === 0) {
+    // Use default image
+    const defaultOption =
+      defaultweatherOptions[weatherData.isDay ? "day" : "night"];
+    weatherOptionUrl = defaultOption.url;
+    weatherOptionCondition = ""; // no specific condition
+    weatherOptionDay = weatherData.isDay;
+  } else {
+    weatherOptionUrl = filteredOptions[0].url;
+    weatherOptionCondition = filteredOptions[0].condition;
+    weatherOptionDay = filteredOptions[0].day;
+  }
+
+  return (
+    <section className="Weather-Card">
+      <p className="Weather-Card__temp">{weatherData.temp.F} &deg; F</p>
+      {weatherOptionUrl && (
+        <img
+          src={weatherOptionUrl}
+          className="Weather-Card__image"
+          alt={`Card showing ${
+            weatherOptionDay ? "day" : "night"
+          }time${weatherOptionCondition ? ` ${weatherOptionCondition}` : ""}`}
+        />
+      )}
     </section>
+  );
 }
 
 export default WeatherCard;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,35 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../../assets/night/cloudy.png", import.meta.url).href,
+  },
+];
+
+export const defaultweatherOptions = {
+  day: {
+    url: new URL("../../assets/day/default.png", import.meta.url).href,
+  },
+  night: {
+    url: new URL("../../assets/night/default.png", import.meta.url).href,
+  },
+};
+
 export const defaultClothingItems = [
   {
     _id: 0,
